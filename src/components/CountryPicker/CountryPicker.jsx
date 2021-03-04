305d1d561bc22e4710269d0f661936ce53f3254e
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-03-04 00:01:37 -0800

    updating

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -15,25 +15,13 @@ const CountryPicker = () => {
     },[setFetchedCountries]);
         console.log(fetchedCountries)
     return (
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-        <div>
-        
-        </div>
-=======
->>>>>>> Stashed changes
         <FormControl className = {styles.fromControl}>
            <NativeSelect deafultValue="">
            <option value = 'global'>Global</option>
            {fetchedCountries.map((country ,i) => <option key = {i} value = {country}>{country}</option>)}
            </NativeSelect>
         </FormControl>
-<<<<<<< Updated upstream
-=======
->>>>>>> c0cbb9487f36715fd3f8173cf159836645596184
->>>>>>> Stashed changes
     )
-}
+    }
 
 export default CountryPicker
