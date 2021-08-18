7dec136f1f456590fca273aad875e4623019a26c
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-08-19 02:12:58 +0500

    Class Component Remove and Functional Component Added

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
-import React from 'react'
-import {Cards , Chart , CountryPicker} from './components'
-import styles from './App.module.css';
-import {fetchData} from './api'
-import CornaImage from './images/image.png'
+import React from "react";
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+import CornaImage from "./images/image.png";
+import { useState } from "react";
+import { useEffect } from "react";
 
-class App extends React.Component{
-  state = {
-    data: {},
-    country : "",
-  }
-async componentDidMount() {
-  const fetchedData = await fetchData();
-  this.setState({data:fetchedData});
-}
+const App = () => {
+  const [data, setData] = useState({});
+  const [country, setCountry] = useState("");
 
-handleCountryChange = async (country) => {
-  const fetchedData = await fetchData(country);
-  this.setState({data:fetchedData , country : country});
-}
+  useEffect(() => {
+    fetchData().then((res) => {
+      console.log(res);
+      setData(res);
+    });
+  }, []);
 
-  render() 
-  
-{
-  const {data , country} = this.state;
-    return(
-      <div  className= {styles.container}>
-      <img  className = {styles.image} src ={CornaImage} alt = "Covid-19 "/ >
-        <Cards data ={data}/>
-        <CountryPicker handleCountryChange = {this.handleCountryChange}/>
-        <Chart data = {data} country = {country}/>
-       
+  const handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    setData(fetchedData);
+    setCountry({ country: country });
+  };
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={CornaImage} alt="Covid-19 " />
+      <Cards data={data} />
+      <CountryPicker handleCountryChange={handleCountryChange} />
+      <Chart data={data} country={country} />
     </div>
-     )
-  }
-}
+  );
+};
 
 export default App;
